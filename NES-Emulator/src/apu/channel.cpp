3814533782dea3b5ac2e2f118fe723d2d732dffb
Akash <akash.munagala@gmail.com>
2018-08-03 18:13:37 +0530

    Fix Typos in lengthCounterTable

--- NES-Emulator/src/apu/channel.cpp
+++ NES-Emulator/src/apu/channel.cpp
@@ -4,8 +4,8 @@
 Channel::Channel(uint8_t* registerStart, Audio& audio) : audio(audio),
 cylesPerSample(1.0 / NTSC_PERIOD / audio.sampleRate), registerStart(registerStart) {};
 
-const std::array<uint8_t, 0x20> Channel::lengthCounterTable{{10, 54, 20,  2, 40,  4, 80,  6, 60,  8, 60, 10,
-14, 12, 26, 14, 12, 16, 24, 18, 48, 20, 96, 22, 92, 24, 72, 26, 16, 28, 32, 30}};
+const std::array<uint8_t, 0x20> Channel::lengthCounterTable{{10, 254, 20, 2, 40, 4, 80, 6, 160, 8,
+60, 10, 14, 12, 26, 14, 12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30}};
 
 // Useful Functions
 void Channel::queueAudio() {
