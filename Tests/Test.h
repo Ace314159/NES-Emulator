3d9c19cda85f60c7edf856864f444fc2f45ac141
Akash <akash.munagala@gmail.com>
2019-06-09 12:27:19 -0700

    Add Google Test

--- Tests/Test.h
+++ Tests/Test.h
@@ -6,9 +6,14 @@ class Test {
 protected:
 	NESTest nes;
 public:
-	explicit Test(const std::string& testFileName);
+	explicit Test(const std::filesystem::path testFileName) : nes(testFileName.string()) {};
 
-	void makeTest();
+	inline void makeTest() {
+		while(glfwGetKey(this->nes.ppu.window.window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+			this->nes.tick();
+			this->nes.dumpScreen();
+		}
+	}
 	virtual bool passed() = 0;
 };
 
