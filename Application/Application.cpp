ffa1196289b889d252c894439f4a1ac0374c3303
Akash <akash.munagala@gmail.com>
2019-06-21 11:49:08 -0700

    Store NES object in heap

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -5,18 +5,18 @@
 #include <windows.h>
 
 int main(int argc, char* argv[]) {
-	cout << std::hex;
+	// cout << std::hex;
 
 	try {
-		NES nes("ROMs/donkey kong.nes");
+		auto nes = std::make_unique<NES>("Legend of Zelda, The (U) (PRG1) [!].nes");
 
 		HRSRC paletteResource = ::FindResource(nullptr, MAKEINTRESOURCE(IDR_NTSCPALETTE), L"PALETTE");
 		HGLOBAL paletteResourceData = ::LoadResource(nullptr, paletteResource);
 		Color* paletteData = static_cast<Color*>(::LockResource(paletteResourceData));
-		nes.ppu.setPaletteTable(paletteData);
+		nes->ppu.setPaletteTable(paletteData);
 
-		while(nes.isRunning()) {
-			nes.tick();
+		while(nes->isRunning()) {
+			nes->tick();
 		}
 	} catch(const std::runtime_error& e) {
 		cout << e.what() << endl;
